6b3387c00cf0979173db8ba5e3112d24b9ea4e30
LeeBumSeok <qpwoeiru6486@gmail.com>
2020-11-30 04:57:12 +0900

    Success, but do not post due to assignment submission period

--- AVL/AVL_skeleton_cpp.cpp
+++ AVL/AVL_skeleton_cpp.cpp
@@ -5,8 +5,6 @@
  */
 
 #include <stdio.h>
-#include <iostream>
-using namespace std;
 
 /**
  * Node represents a single node in AVL tree.
@@ -24,55 +22,7 @@ typedef Node *Tree;
  */
 Node *getNode() {
   /* write your code here */
-  Node *tmp = new Node;
-  tmp->key =0;
-  tmp->bf=0;
-  tmp->left = NULL;
-  tmp->right = NULL;
-  return tmp;
 }
-
-//find height of tree
-int height(Node* tmp){
-  //case of empty tree
-  if(tmp ==NULL){
-    return -1;
-  }
-  int left = height(tmp->left);
-  int right = height(tmp->right);
-  if(left>right){
-    return left +1;
-  }
-  else{
-    return right+1;
-  }
-}
-
-//find maximal value of left child
-int maxNode(Node* tmp){
-  while(tmp->right!=NULL){
-    tmp = tmp->right;
-  }
-  return tmp->key;
-}
-
-//find minimal value of rihgt child
-int minNode(Node* tmp){
-  while(tmp->left!=NULL){
-    tmp = tmp->left;
-  }
-  return tmp->key;
-}
-
-//find number of node each right child, left child
-int noNodes(Node* tmp ){
-  if(tmp == NULL){
-     return 0;}
-  else{
-    return noNodes(tmp->left) +noNodes(tmp->right)+1;
-  }
-}
-
 
 /**
  * updateBF updates balancing factors of nodes in T
@@ -135,34 +85,6 @@ void rotateRL(Tree *T, Node *x, Node *p) {
  */
 Node *insertBST(Tree *T, int newKey) {
   /* write your code here */
-  // find lotation of node
-  Node* q =NULL; Node* p = *T;
-  while(p!=NULL){
-    if(newKey==p->key){
-      return p;
-    }
-    q=p;
-    if(newKey<p->key){
-      p=p->left;
-    }
-    else{
-      p=p->right;
-    }
-  }
-  // make node with new key
-  Node* newNode = getNode();
-  newNode->key = newKey;
-  // insert
-  if(*T==NULL){
-    *T = newNode;
-  }
-  else if(newKey<q->key){
-    q->left = newNode;
-  }
-  else{
-    q->right= newNode;
-  }
-  return newNode;
 }
 
 /**
@@ -179,8 +101,6 @@ void insertAVL(Tree *T, int newKey) {
    * print "RL" if rotates RL
    */
   /* write your code here */
-  insertBST(T,newKey);
-
 }
 
 /**
@@ -190,109 +110,9 @@ void insertAVL(Tree *T, int newKey) {
  * @param deleteKey: a key to delete
  * @return the parent node of the deleted node
  */
+}
 Node *deleteBST(Tree *T, int deleteKey) {
   /* write your code here */
-  //find lotation of node
-  Tree q =NULL; Tree p = *T;
-  while(p!=NULL){
-    if(deleteKey== p->key){
-      break;
-    }
-    q=p;
-    if(deleteKey< p->key){
-      p=p->left;
-    }
-    else{
-      p=p->right;
-    }
-  }
-  if(p==NULL){
-    return NULL;
-  }
-  //no child
-  if(p->left==NULL && p->right==NULL){
-    //case of root
-    if(p==*T){
-      q =NULL;
-      return NULL;
-    }
-    else{
-      if(q->left==p){
-        q->left ==NULL;
-      }
-      else{
-        q->right ==NULL;
-      }
-    }
-  }
-  // one child
-  if(p->left==NULL || p->right==NULL){
-    // case of root
-    if(q==NULL){
-      if(p->left!=NULL){
-        *T = p->left;
-      }
-      else{
-        *T=p->right;
-      }
-    }
-    else {
-    if(p->left!=NULL){
-      if(q->left==p){
-        q->left = p->left;
-      }
-      else{
-        q->right = p->left;
-      }
-    }
-    else{
-      if(q->left==p){
-        q->left = p->right;
-      }
-      else{
-        q->right=p->right;
-      }
-    }
-    }
-    return q;
-  }
-  //two child
-  else{
-    //0 is left, 1 is right
-    bool flag;
-    int r;
-    int leheight=height(p->left);
-    int riheight=height(p->right);
-    if(leheight>=riheight){
-      r = maxNode(p->left);
-      flag = 0;
-    }
-    //to change node with child who has higher height
-    else if(riheight>leheight){
-      r = minNode(p->right);
-      flag = 1;
-    }
-    else{
-      if(noNodes(p->left)>=noNodes(p->right)){
-        r = maxNode(p->left);
-        flag = 0;
-      }
-      else{
-        r = minNode(p->right);
-        flag=1;
-      }
-    }
-    //change key for proper value
-    p->key = r;
-    //delete node according to flag value
-    if(flag==0){
-      deleteBST(&p->left, r);
-    }
-    else{
-      deleteBST(&p->right, r);
-    }
-  }
-  return q;
 }
 
 /**
@@ -309,7 +129,6 @@ void deleteAVL(Tree *T, int deleteKey) {
    * print "RL" if rotates RL
    */
   /* write your code here */
-  deleteBST(T,deleteKey);
 }
 
 /**
@@ -318,11 +137,6 @@ void deleteAVL(Tree *T, int deleteKey) {
  */
 void inorderAVL(Tree T) {
   /* write your code here */
-  if(T!=NULL){
-    inorderAVL(T->left);
-    cout << T->key << " ";
-    inorderAVL(T->right);
-  }
 }
 
 int main() {
@@ -346,3 +160,4 @@ int main() {
   // redeletion
   for (int i=19; 0<=i; i--) { printf("%d ", testcase[i]); deleteAVL(&T, testcase[i]); printf(" : "); inorderAVL(T); printf("\n"); }
 }
+
